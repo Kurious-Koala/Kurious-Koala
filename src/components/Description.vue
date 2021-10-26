9328655fc41e9309b266aede6aa76b76e9878cb8
Eleni <helena.aslanidou@gmail.com>
2021-10-26 21:29:33 +0200

    📣 added component Description in Aboout page

---
+++ src/components/Description.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="description">
+    <p>
+      Hello! We are a team of Developers and Engineers. Our passion for coding
+      combined with our love for children made us to create this online game.
+      Remember that playing is fun but playing and learning is AWESOME! ....
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.description {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: #e8cfcf;
+  border-radius: 10px;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div v-if="ShowDescription">
+    <Description />
   </div>
 </template>
+
+<script>
+import Description from "../components/Description.vue";
+export default {
+  name: "About",
+  components: {
+    Description,
+  },
+  data() {
+    return {
+      text: "become a member and take -10%",
+      ShowDescription: true,
+    };
+  },
+  methods: {
+    handleDescription() {
+      this.ShowDescription = !this.ShowDescription;
+    },
+  },
+};
+</script>
