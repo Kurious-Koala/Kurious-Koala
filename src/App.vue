2f312bf8c264a9c0fa1cc0cf89865d75c54fdbc6
Eleni <helena.aslanidou@gmail.com>
2021-11-04 22:10:34 +0100

    added koala img-changed Navbar-added dropdown lang
    changed the styling of Navbar - addde welcome message - created dropdown for languages

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,20 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-    <router-link to="/">Levels</router-link> |
-    <router-link to="/login">Login</router-link> |
+  <div class="koala">
+    <img class="koala" alt="koala" src="./assets/KoalaNavbar.png" />
   </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-link to="/levels">Levels</router-link>
+    <router-link to="/login">Login</router-link>
+    <select v-model="currentLocale">
+      <option v-for="locale in locales" v-bind:key="locale.id">
+        {{ locale.name }}
+      </option>
+    </select>
+    <p class="welcome-msg">{{ message[currentLocale] }}</p>
+  </div>
+  <!-- <img class="globe" alt="globe" src="./assets/globe.svg" /> --> 
   <router-view />
   <Footer />
 </template>
@@ -13,20 +23,41 @@
 // @ is an alias to /src
 import Footer from "@/components/Footer.vue";
 export default {
-  name: "App", 
+  name: "App",
   components: {
     Footer,
+  },
+  el: "#app",
+  data() {
+    return {
+      message: {
+        en: "Welcome to kurious Koala",
+        gr: "Καλώς ήρθατε στο kurious Koala",
+        sv: "Välkomna till kurious Koala",
+      },
+      currentLocale: "en",
+      locales: [
+        { id: "en", name: "English" },
+        { id: "gr", name: "Greek" },
+        { id: "sv", name: "Swedish" },
+      ],
+    };
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Henny Penny", cursive;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
+  font-size: 24px;
+}
+
+.koala {
+  width: 80px;
+  height: 80px;
+  margin: auto 240px;
 }
 
 #nav {
@@ -35,10 +66,22 @@ export default {
 
 #nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
+  padding: 0 20px;
+}
+
+.koala:hover {
+  color: #96d19b;
 }
 
 #nav a.router-link-exact-active {
-  color: #b94242;
+  color: #96d19b;
+}
+
+.welcome-msg {
+  font-family: "Ribeye Marrow";
+  color: #087d12;
+  font-size: 30px;
+  padding-top: 50px;
 }
 </style>
